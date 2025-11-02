416f93215583ab07e9b09ec1a772881ed8c0dfa1
Pavithra Sree Pasumarthi <ppavithrasree@gmail.com>
2025-11-02 07:09:44 +0000

    nkfdjnh

---
+++ week9/app.py
@@ -0,0 +1,7 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+@app.route('/')
+def hello_world():
+    return render_template("order.html")
+if __name__=='__main__':
+    app.run(debug=True,host='127.0.0.1',port-'5000')
